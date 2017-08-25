88a7466257031205480f9d7efb926d8ffc46f7b3
shjman1\shjman11 <shjman@outlook.com>
2017-08-25 07:14:07 +0300

    task5 done

---
+++ src/myPackage/task5.java
@@ -0,0 +1,28 @@
+package myPackage;
+import java.util.Scanner;
+public class task5{
+
+        public static void main(String[] args) {
+            Scanner scany = new Scanner(System.in);
+            System.out.println("Введите предпологаемое палидромное слово");
+            String s = scany.next();
+            task5 go=new task5();
+            go(s);
+        }
+
+        public static String reverse(String s) {
+            String q = "";
+            for (int i = s.length() - 1; i >= 0; i--)
+                q += s.charAt(i);
+            return q;
+
+        }
+        public static void go(String s) {
+            if (s.equals(reverse(s))) {
+                System.out.println("это палиндромность");
+            } else {
+                System.out.println("это несовсем палиндромность");
+            }
+
+        }
+}
