08115160fd82a38b9686f25449b9fb69025f4df2
shjman1\shjman11 <shjman@outlook.com>
2017-08-25 13:05:47 +0300

    task4 refactor

--- src/myPackage/task4.java
+++ src/myPackage/task4.java
@@ -7,19 +7,16 @@ public class task4 {
         System.out.println("введите сначало одно число,а после \"ентера\" второе ");
         int a = in.nextInt();
         int b = in.nextInt();
-
         System.out.println("наибольший общий делитель "+nod(a,b)+"\n"+"наименьшее общее кратное "+nok(a,b));
-
     }
-
-        public static int nod(int a,int  b) {
-            if (b == 0)
-                return Math.abs(a);
-            return nod(b, a % b);
+        public static int nod(int a,int  b) {//рекурсивный метод вызывающий сам себя пока числа не поделятся без остатка
+            if (b == 0){
+                return Math.abs(a);}//берем по модулю т.к для -5 и 15 наибольший нод это 5
+            else{return nod(b, a % b);}
         }
         public static int nok(int a, int b){
         int z;
-        z=a/nod(a,b)*b;
+        z=a/nod(a,b)*b;// вычисляем через НОД в одну формулу
         return z;
         }
 }
