84984a12810f7703afef19157470ff5bb8d23fc4
shjman1\shjman11 <shjman@outlook.com>
2017-08-25 07:05:41 +0300

    task2 done

--- src/myPackage/task2.java
+++ src/myPackage/task2.java
@@ -1,4 +1,21 @@
 package myPackage;
-
+import java.util.Scanner;
 public class task2 {
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Введите число N- предел(включительно) вычисления чисел фибонначи  ");
+            int g = in.nextInt();
+
+            int a = 1, b = 1;
+            System.out.print(a);
+            int summa = 1;
+            while (summa <= g) {
+                System.out.print(" " + summa);
+                summa = a + b;
+                a = b;
+                b = summa;
+
+
+            }
+        }
 }
